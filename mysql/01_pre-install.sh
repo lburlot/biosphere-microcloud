4e7287265fe49cb33fa4c389423c3e668bd27742
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-07-26 19:03:49 +0200

    Run docker without selinux (it doesn't start on most IFB clouds).

--- mysql/01_pre-install.sh
+++ mysql/01_pre-install.sh
@@ -9,3 +9,14 @@ groupadd docker
 usermod -aG docker root
 usermod -aG docker centos
 
+# Disable selinux for docker
+CONFIG_FILE=/etc/sysconfig/docker
+cp -p ${CONFIG_FILE} ${CONFIG_FILE}.orig
+cat > ${CONFIG_FILE} << "EOF"
+OPTIONS='--log-driver=journald --signature-verification=false'
+if [ -z "${DOCKER_CERT_PATH}" ]; then
+    DOCKER_CERT_PATH=/etc/docker
+fi
+EOF
+chmod 0644 ${CONFIG_FILE}
+
