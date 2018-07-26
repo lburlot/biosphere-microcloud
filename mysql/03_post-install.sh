63a24daed4cac44c363c721d628a8fcc50c70c4e
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-07-26 19:15:22 +0200

    Move docker configuration code from 01_pre-install.sh to 03_post-install.sh.

--- mysql/03_post-install.sh
+++ mysql/03_post-install.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -xe
 
+# Add docker group and add ourselves and centos in
+groupadd docker
+usermod -aG docker root
+usermod -aG docker centos
+
+# Disable selinux for docker
+CONFIG_FILE=/etc/sysconfig/docker
+cp -p ${CONFIG_FILE} ${CONFIG_FILE}.orig
+cat > ${CONFIG_FILE} << "EOF"
+OPTIONS='--log-driver=journald --signature-verification=false'
+if [ -z "${DOCKER_CERT_PATH}" ]; then
+    DOCKER_CERT_PATH=/etc/docker
+fi
+EOF
+chmod 0644 ${CONFIG_FILE}
+
 # Start Docker :
 systemctl start docker
 
