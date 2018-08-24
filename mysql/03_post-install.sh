55571c83d4680bb7f393f6ac8a8937f76f1af130
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-08-24 19:26:26 +0200

    Use random password for MySQL root user.
    We need to move the code to start the container (and therefore the code
    that depend on it) in 04_deployment so the password is dynamically
    created.

--- mysql/03_post-install.sh
+++ mysql/03_post-install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -xe
 
+########################
+# Docker configuration #
+########################
+
 # Add docker group and add ourselves and centos in
 groupadd docker
 usermod -aG docker root
@@ -16,39 +20,9 @@ fi
 EOF
 chmod 0644 ${CONFIG_FILE}
 
-# Start Docker :
+# Start Docker
 systemctl start docker
 
 # Pull mysql image
 docker pull mysql:5.7
-
-# Run mysql container and expose port (we need to start it before installing the UDF)
-CONTAINER_NAME=mysql01
-docker run --detach --name=${CONTAINER_NAME} --env="MYSQL_ROOT_PASSWORD=mypassword" -p 80:80 -p 3306:3306 mysql:5.7
-
-##################
-# MicroScope UDF #
-##################
-
-# Install some packages inside the container
-PACKAGES_FOR_UDF="build-essential autoconf automake libtool procps libmysqlclient-dev"
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${PACKAGES_FOR_UDF}"
-
-# Download, copy the files and uncompress the sources
-curl -o lib_mysqludf_Sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_Sequtils-0.4.7.tar.gz
-docker cp lib_mysqludf_Sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
-docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_Sequtils-0.4.7.tar.gz
-
-# Compilation & installation (we need the password)
-MYSQL_PASSWORD=mypassword
-docker exec ${CONTAINER_NAME} sh -c "\
-cd lib_mysqludf_Sequtils-0.4.7/src && \
-autoreconf -fi &&\
-./configure --with-mysqlbin='mysql -u root --password=${MYSQL_PASSWORD}' && make && make install
-"
-# Remove file and folder
-docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_Sequtils-0.4.7.tar.gz lib_mysqludf_Sequtils-0.4.7
-
-# Remove packages
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${PACKAGES_FOR_UDF} && apt-get -y autoremove"
 
