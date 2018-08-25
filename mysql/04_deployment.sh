b8277840198b4077f7561f77485da117376e7161
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-08-25 12:42:58 +0200

    Another attempt to use MYSQL_PASSWORD.
    Arghhh!!!

--- mysql/04_deployment.sh
+++ mysql/04_deployment.sh
@@ -10,7 +10,7 @@ CONTAINER_NAME=mysql01
 docker run --detach --name=${CONTAINER_NAME} --env="MYSQL_RANDOM_ROOT_PASSWORD=true" -p 3306:3306 mysql:5.7
 
 # Get the generated password
-export MYSQL_PASSWORD=$(docker logs ${CONTAINER_NAME} 2>&1 | grep GENERATED | awk '{print $NF}')
+MYSQL_PASSWORD=`docker logs ${CONTAINER_NAME} 2>&1 | grep GENERATED | awk '{print $NF}'`
 
 # Export password
 ss-set mysqlRootPassword "${MYSQL_PASSWORD}"
@@ -21,25 +21,25 @@ ss-set mysqlRootPassword "${MYSQL_PASSWORD}"
 ##################
 
 # Install some packages inside the container
-PACKAGES_FOR_UDF="build-essential autoconf automake libtool procps libmysqlclient-dev"
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${PACKAGES_FOR_UDF}"
+#PACKAGES_FOR_UDF="build-essential autoconf automake libtool procps libmysqlclient-dev"
+#docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${PACKAGES_FOR_UDF}"
 
 # Download, copy the files and uncompress the sources
-curl -o lib_mysqludf_Sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_Sequtils-0.4.7.tar.gz
-docker cp lib_mysqludf_Sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
-docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_Sequtils-0.4.7.tar.gz
+#curl -o lib_mysqludf_Sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_Sequtils-0.4.7.tar.gz
+#docker cp lib_mysqludf_Sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
+#docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_Sequtils-0.4.7.tar.gz
 
 # Compilation & installation (we need the password)
-docker exec ${CONTAINER_NAME} sh -c "\
-cd lib_mysqludf_Sequtils-0.4.7/src && \
-autoreconf -fi &&\
-./configure --with-mysqlbin='mysql -u root --password=${MYSQL_PASSWORD}' && make && make install
-"
+#docker exec ${CONTAINER_NAME} sh -c "\
+#cd lib_mysqludf_Sequtils-0.4.7/src && \
+#autoreconf -fi &&\
+#./configure --with-mysqlbin='mysql -u root --password=${MYSQL_PASSWORD}' && make && make install
+#"
 # Remove file and folder
-docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_Sequtils-0.4.7.tar.gz lib_mysqludf_Sequtils-0.4.7
+#docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_Sequtils-0.4.7.tar.gz lib_mysqludf_Sequtils-0.4.7
 
 # Remove packages
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${PACKAGES_FOR_UDF} && apt-get -y autoremove"
+#docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${PACKAGES_FOR_UDF} && apt-get -y autoremove"
 
 ####################
 # Backend is ready #
