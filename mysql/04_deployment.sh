eb6947f2addf25b5bb9967cdfcc7d441706f305f
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-10-06 02:57:10 +0200

    Update to the new distribution of lib_mysqludf_Sequtils

--- mysql/04_deployment.sh
+++ mysql/04_deployment.sh
@@ -23,22 +23,21 @@ ss-set mysqlRootPassword ${MYSQL_PASSWORD}
 ##################
 
 # Install some packages inside the container
-PACKAGES_FOR_UDF="build-essential autoconf automake libtool procps libmysqlclient-dev"
+PACKAGES_FOR_UDF="build-essential procps libmysqlclient-dev"
 docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${PACKAGES_FOR_UDF}"
 
 # Download, copy the files and uncompress the sources
-curl -o lib_mysqludf_Sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_Sequtils-0.4.7.tar.gz
-docker cp lib_mysqludf_Sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
-docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_Sequtils-0.4.7.tar.gz
+curl -o lib_mysqludf_sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_sequtils-0.4.7.tar.gz
+docker cp lib_mysqludf_sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
+docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_sequtils-0.4.7.tar.gz
 
 # Compilation & installation (we need the password)
 docker exec ${CONTAINER_NAME} sh -c "\
-cd lib_mysqludf_Sequtils-0.4.7/src && \
-autoreconf -fi &&\
+cd lib_mysqludf_sequtils-0.4.7/ && \
 ./configure --with-mysqlbin='mysql -u root --password=${MYSQL_PASSWORD}' && make && make install
 "
 # Remove file and folder
-docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_Sequtils-0.4.7.tar.gz lib_mysqludf_Sequtils-0.4.7
+docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_sequtils-0.4.7.tar.gz lib_mysqludf_sequtils-0.4.7
 
 # Remove packages
 docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${PACKAGES_FOR_UDF} && apt-get -y autoremove"
