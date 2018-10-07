3891a8575c669282307c5225c73bbd7eb235a416
Mathieu Dubois <mdubois@genoscope.cns.fr>
2018-10-07 16:10:05 +0200

    Store lib_mysqludf_sequtils version in a variable and rename another variable.

--- mysql/04_deployment.sh
+++ mysql/04_deployment.sh
@@ -20,25 +20,28 @@ ss-set mysqlRootPassword ${MYSQL_ROOT_PASSWORD}
 # MicroScope UDF #
 ##################
 
+# Version to use
+MIC_UDF_VERSION=0.4.7
+MIC_UDF_PACKAGES="build-essential procps libmysqlclient-dev"
+
 # Install some packages inside the container
-PACKAGES_FOR_UDF="build-essential procps libmysqlclient-dev"
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${PACKAGES_FOR_UDF}"
+docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y install ${MIC_UDF_PACKAGES}"
 
 # Download, copy the files and uncompress the sources
-curl -o lib_mysqludf_sequtils-0.4.7.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_sequtils-0.4.7.tar.gz
-docker cp lib_mysqludf_sequtils-0.4.7.tar.gz ${CONTAINER_NAME}:/
-docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_sequtils-0.4.7.tar.gz
+curl -o lib_mysqludf_sequtils-${MIC_UDF_VERSION}.tar.gz https://www.genoscope.cns.fr/agc/ftp/lib_mysqludf_sequtils-${MIC_UDF_VERSION}.tar.gz
+docker cp lib_mysqludf_sequtils-${MIC_UDF_VERSION}.tar.gz ${CONTAINER_NAME}:/
+docker exec ${CONTAINER_NAME} tar -xzf lib_mysqludf_sequtils-${MIC_UDF_VERSION}.tar.gz
 
 # Compilation & installation (we need the password)
 docker exec ${CONTAINER_NAME} sh -c "\
-cd lib_mysqludf_sequtils-0.4.7/ && \
+cd lib_mysqludf_sequtils-${MIC_UDF_VERSION}/ && \
 ./configure && MYSQL_USER=root MYSQL_PASSWORD=${MYSQL_ROOT_PASSWORD} make install
 "
 # Remove file and folder
-docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_sequtils-0.4.7.tar.gz lib_mysqludf_sequtils-0.4.7
+docker exec ${CONTAINER_NAME} rm -rf lib_mysqludf_sequtils-${MIC_UDF_VERSION}.tar.gz lib_mysqludf_sequtils-${MIC_UDF_VERSION}
 
 # Remove packages
-docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${PACKAGES_FOR_UDF} && apt-get -y autoremove"
+docker exec ${CONTAINER_NAME} sh -c "apt-get update && apt-get -y remove ${MIC_UDF_PACKAGES} && apt-get -y autoremove"
 
 ####################
 # Backend is ready #
